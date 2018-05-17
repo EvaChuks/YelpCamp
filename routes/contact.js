f6a75a501de13d2d3010e137f0c87efe66ca9d11
Walter Chuks <walter4chuks@gmail.com>
2018-05-17 16:09:21 +0000

    contact.js update

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,13 @@
 var express             = require("express");
 var router              = express.Router();
-var nodemailer          = require("nodemailer");
+//var nodemailer          = require("nodemailer");
 var request             = require("request");
+
+
+//var mailgun = require("mailgun-js");
+var api_key = process.env.MAILGUN_API_KEY;
+var DOMAIN = process.env.MAILGUN_DOMAIN;
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
 
         // contact form
@@ -28,34 +34,58 @@ router.post("/", function(req, res) {
         req.flash("error", "Captcha Failed");
         return res.redirect("/contact");
       }
-        var smtpTransport = nodemailer.createTransport({
-            service: 'Gmail', 
-            auth: {
-              user: 'walter4chuks@gmail.com',
-              pass: process.env.GMAILPW
-            }
+      
+      // INTERCEPTING THE DATA
+      var name = req.body.name;
+      var email = req.body.email;
+      var phone = req.body.phone;
+      var subject = req.body.subject;
+      var message = req.body.message;
+      
+
+     var data = {
+       from: name + " - " + email, // Email and name from the variables
+       to: process.env.EMAIL,
+       subject: subject,
+       message: message,
+       phone: phone
+      };
+
+      mailgun.messages().send(data, function (error, body) {
+        if(error) {
+          console.log(error);
+        }
+
+         console.log(body);
         });
+        // var smtpTransport = nodemailer.createTransport({
+        //     service: 'Gmail', 
+        //     auth: {
+        //       user: 'walter4chuks@gmail.com',
+        //       pass: process.env.GMAILPW
+        //     }
+        // });
         
-         var mailOptions = {
-            from: 'evaristus.ihezurumba@gmail.com',
-            to: 'evaristus.ihezurumba@gmail.com',
-            replyTo: req.body.email,
-            subject: "Let's Camp contact request from: " + req.body.name,
-            text: 'You have received an email from... Name: '+ req.body.name + ' Phone: ' + req.body.phone + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
-            html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Phone: ' + req.body.phone + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
-        };
+        // var mailOptions = {
+        //     from: 'evaristus.ihezurumba@gmail.com',
+        //     to: 'evaristus.ihezurumba@gmail.com',
+        //     replyTo: req.body.email,
+        //     subject: "Let's Camp contact request from: " + req.body.name,
+        //     text: 'You have received an email from... Name: '+ req.body.name + ' Phone: ' + req.body.phone + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+        //     html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Phone: ' + req.body.phone + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
+        // };
         
-        smtpTransport.sendMail(mailOptions, function(err, info){
-          if(err) {
-            console.log(err)
-            req.flash("error", "Something went wrong... Please try again later!");
-            res.redirect("/contact");
-          } else {
-            req.flash("success", "Your email has been sent, we will respond within 24 hours.");
-            res.redirect("/campgrounds");
+        // smtpTransport.sendMail(mailOptions, function(err, info){
+        //   if(err) {
+        //     console.log(err)
+        //     req.flash("error", "Something went wrong... Please try again later!");
+        //     res.redirect("/contact");
+        //   } else {
+        //     req.flash("success", "Your email has been sent, we will respond within 24 hours.");
+        //     res.redirect("/campgrounds");
             
-          }
-        });
+        //   }
+        // });
     });
 });
 
